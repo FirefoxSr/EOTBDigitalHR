70fe0780bdf0e4d73fc435e2a73b6d26647ef5fa
LilliePad <Lillielindrob@Outlook.com>
2019-11-24 22:59:51 +0100

    added the monsters to the encounters
    I added the monsters json file to the encounters and made it work in the p[ython script

--- Encounter table.py
+++ Encounter table.py
@@ -9,8 +9,10 @@ while (squareCheck != 22):
     num = str(random.randint(1, 6))
 
     #opens the json file and saves it to a variable
-    jsonfile = open('encountertable.json')
-    encounter = json.load(jsonfile)
+    jsonEcnounterTable = open('encountertable.json')
+    encounter = json.load(jsonEcnounterTable)
+    jsonMonsters= open('monsters.json')
+    monsters = json.load(jsonMonsters)
 
     #asks the player to input the square they landed on and turns that integer into a string
     while (check == False):
@@ -31,6 +33,19 @@ while (squareCheck != 22):
     
     #prints the encounter to the console
     print(encounter["EncounterTable"][square]["Dice" + num])
+
+    #this gets the enemy stats out of the json file
+    enemyStats = monsters["monsters"][enemy]
+    startingEnergy = monsters["monsters"][enemy]["starting_energy"]
+    print(startingEnergy + ' starting energy')
+    usingCards = monsters["monsters"][enemy]["using_cards"]
+    print(usingCards + ' using cards')
+    charging = monsters["monsters"][enemy]["charging"]
+    print(charging + ' charging')
+    special = monsters["monsters"][enemy]["special"]
+    print(special)
+
+    print(enemyStats)
 
     if (squareCheck == 22):
         programStop = input('Do you want to quit? y/n: ')
