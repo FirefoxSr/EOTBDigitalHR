53881d869449c07c64376186440942a6062febf5
LilliePad <Lillielindrob@Outlook.com>
2019-11-24 19:50:30 +0100

    Encounter
    For now finished, until we implement it into the main gui.

--- Encounter table.py
+++ Encounter table.py
@@ -1,13 +1,40 @@
-import json
-jsonfile = open('encountertable.json')
-encounter = json.load(jsonfile)
-print(encounter)
-square = input('What square did you land on? ')
-Area = input('What area are you in? ')
-num = input('enter your dice roll here: ')
-print('Hallo')
+squareCheck = 0
 
+while (squareCheck != 22):
+    import json
+    import random
 
-print(num)
-print(encounter)
-print(encounter["EncounterTable"][Area][square]["Dice" + num])
+    check = False
+    #generate the random dice roll here
+    num = str(random.randint(1, 6))
+
+    #opens the json file and saves it to a variable
+    jsonfile = open('encountertable.json')
+    encounter = json.load(jsonfile)
+
+    #asks the player to input the square they landed on and turns that integer into a string
+    while (check == False):
+        square = input('What square did you land on? ')
+        squareCheck = int(square)
+        square = 'playerSquare' + str(square)
+        if (squareCheck > 22 or squareCheck == 0):
+            check = False
+            print('That square does not exist')
+        else:
+            check = True
+            
+
+    
+
+    #saves the encountered enemy to a variable so the program can use it
+    enemy = encounter["EncounterTable"][square]["Dice" + num]
+    
+    #prints the encounter to the console
+    print(encounter["EncounterTable"][square]["Dice" + num])
+
+    if (squareCheck == 22):
+        programStop = input('Do you want to quit? y/n: ')
+        if (programStop == 'y'):
+            squareCheck = 22
+        else:
+            squareCheck = 0
