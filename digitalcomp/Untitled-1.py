21ae5da2ff732fabcc72546901e2319a80467da0
LilliePad <Lillielindrob@Outlook.com>
2019-12-10 09:55:35 +0100

    Removed non branch related items
    I removed the items that do not apply to this branch

--- digitalcomp/Untitled-1.py
+++
@@ -1,6 +0,0 @@
-PImage img = loadImage(background.jpeg)
-int x=img.width
-int y=img.height
-
-println("x="+x)
-println("y="+y)
--- gui/map.py
+++
@@ -1,9 +0,0 @@
-def setup():
-    global map
-    fullScreen()
-    map = loadImage("map.png")
-
-def draw():
-    global map
-    background(0);
-    image(map,0,0)
