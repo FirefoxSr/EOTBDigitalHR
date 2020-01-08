7df9c7edbf0f9fc831a50bf7d8e1a1d8e2b2eb82
LilliePad <Lillielindrob@Outlook.com>
2020-01-08 22:50:59 +0100

    Added the scrolling background

--- Gui/backgroundScroll.py
+++ Gui/backgroundScroll.py
@@ -1,13 +1,14 @@
 def setup():
     global backGround, x
-    frameRate(60)
+    frameRate(90)
     backGround = loadImage("Background.png")
-    x = 0
+    image(backGround,0,0)
+    x = 2560
 def draw():
     global backGround, x
-
+    
     x = x - 1
     image(backGround, x, height // 2)
     print(x)
-    if (x < -1080):
-        x = width // 2
+    if (x < 0):
+        x = 2560
